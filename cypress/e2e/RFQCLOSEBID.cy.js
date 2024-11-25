fb2042b84652811cc38a7c3e1c5107847fbb0fb3
priya <sonaprishi@gmail.com>
2024-11-25 15:02:34 +0530

    move

--- cypress/e2e/RFQCLOSEBID.cy.js
+++ cypress/e2e/RFQCLOSEBID.cy.js
@@ -2,13 +2,13 @@
 import 'cypress-file-upload';
 describe("Edit Profile", () => {
     beforeEach(() => {
-      cy.visit("https://pev3qaapp.azurewebsites.net/aspl/");
+      cy.visit("https://peqaapp.azurewebsites.net/aspl");
       Cypress.on("uncaught:exception", (err, runnable) => {
         return false;
       });
     
       cy.get('#username').type("priya.chauhan@agileapt.com"); 
-      cy.get("#password").type("4321");
+      cy.get("#password").type("Vv@1234");
       cy.get('.col-sm-8 > .btn').click();  
      
 
@@ -25,10 +25,10 @@ describe("Edit Profile", () => {
         cy.get('#txtrfqSubject').type("Close bid");
         //calendar
 
-        cy.get('.col-md-4 > .input-group > .input-group-btn > #btncal').click("");
-        cy.get('[style="display: block; top: 422.508px; left: 368.514px;"] > .datetimepicker-days > .table-condensed > tbody > :nth-child(5) > :nth-child(4)').click("");
-        cy.get('[style="display: block; top: 422.509px; left: 380.514px;"] > .datetimepicker-hours > .table-condensed > tbody > tr > td > .active').click("");
-        cy.get('[style="display: block; top: 422.509px; left: 380.514px;"] > .datetimepicker-minutes > .table-condensed > tbody > tr > td > :nth-child(12)').click("");
+        cy.get('.col-md-3 > .input-group > .input-group-btn > .btn').click();
+        cy.get('[style="left: 663.413px; display: block; top: 430.7px;"] > .datetimepicker-days > .table-condensed > tbody > :nth-child(6) > :nth-child(6)').click();
+        cy.get('[style="left: 672.438px; display: block; top: 430.7px;"] > .datetimepicker-hours > .table-condensed > tbody > tr > td > :nth-child(20)').click();
+        cy.get('[style="left: 672.438px; display: block; top: 430.7px;"] > .datetimepicker-minutes > .table-condensed > tbody > tr > td > :nth-child(12)').click();
         cy.get('.has-error > .col-md-3 > .input-group > .input-group-btn > .btn > .fa').click();
         cy.get('#txtrfqdescription').type("type an  error ");
         cy.get('#file1').click().wait(6000);
@@ -44,10 +44,61 @@ describe("Edit Profile", () => {
 
         //approver
 
-        cy.get('#txtApprover').type("ABC{Enter}");
-        cy.get(':nth-child(4) > .col-md-1 > #Addbtn1 > .fa').click();
+         
+        cy.get('#file1').click().wait(8000);
+        
+       
+     
+        cy.get('#txtApprover').type("priya chauhan");
+        cy.get('.typeahead > .active > a').click();
+        cy.get(':nth-child(7) > :nth-child(1) > :nth-child(4) > .col-md-1 > #Addbtn1 > .fa').click();
+        cy.get('.row > .col-md-offset-5 > .yellow').click();
+
+
+        //step2 Items/Services
+
+        cy.get('#txtshortname').should("be.visible").should("not.be.disabled").type("GHIJKLMNOPQRs");
+        cy.get('#txtbiddescriptionP').should("be.visible").should("not.be.disabled").type("GHIJKLMNOPQRs");
+        cy.get('#txtquantitiy').should("be.visible").should("not.be.disabled").type("65");
+        cy.get('#txtUOM').should("be.visible").should("not.be.disabled").type("Number{Enter}");
+        cy.get('#txtedelivery').should("be.visible").should("not.be.disabled").type("Noida");
+        cy.get('#add_or').click();
+        cy.get('.row > .col-md-offset-5 > .yellow').click();
+       
+
+
+        //step 3
+
+        cy.get(':nth-child(1) > .panel-heading > .panel-title > .accordion-toggle').click();
+        cy.get('#chkTerms18').click();
+        cy.get('#chkTerms3').click();
+        cy.get('#chkTerms4').click();
+        cy.get('#chkTerms6').click();
+        cy.get('#chkTerms8').click();
+        cy.get('#chkTerms14').click();
+        cy.get('#chkTerms16').click();
+        cy.get('#chkTerms17').click();
 
         cy.get('.row > .col-md-offset-5 > .yellow').click();
+
+
+        //step4
+
+        cy.get('#txtVendorGroup').type("demo");
+        cy.get('.col-md-8 > .typeahead > .active > a').click();
+        cy.get('#chkvender460').click();
+        cy.get('#chkvender289').click();
+        cy.get('#chkvender474').click();
+        cy.get('.row > .col-md-offset-5 > .green').click();
+
+        //preview
+        cy.get('.col-md-offset-11 > .green').click();
+
+
+        //popup
+
+        cy.get('.bootbox > .modal-dialog > .modal-content > .modal-footer > .btn').click();
+
 
         
        
