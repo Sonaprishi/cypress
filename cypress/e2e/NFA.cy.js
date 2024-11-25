b0c9403d09fdc366bdc2106b86a271d6c68b516e
priya <sonaprishi@gmail.com>
2024-11-25 13:14:49 +0530

    NFA

--- cypress/e2e/NFA.cy.js
+++ cypress/e2e/NFA.cy.js
@@ -1,74 +1,52 @@
-
 import 'cypress-file-upload';
+
 describe("Edit Profile", () => {
-    beforeEach(() => {
-      cy.visit("https://pev3qaapp.azurewebsites.net//aspl/");
-      Cypress.on("uncaught:exception", (err, runnable) => {
-        return false;
-      });
-    
-      cy.get('#username').should("not.be.disabled").type("priya.chauhan@agileapt.com"); 
-      cy.get("#password").should("not.be.disabled").type("4321");
-      cy.get('.col-sm-8 > .btn').click();  
+  beforeEach(() => {
+    cy.visit("https://peqaapp.azurewebsites.net/aspl");
+    Cypress.on("uncaught:exception", () => false);
 
- 
-     });
-    it("Usr FA", () => {
-      var file = "../fixture/example.json"
-        cy.get('.sidebar-toggler-wrapper > .sidebar-toggler').click();
-        cy.get('#ulMain > :nth-child(3) > [href="javascript:;"]').click();
-
-
-        //NFA Request
-
-        cy.get('#MenuHeader1 > :nth-child(6) > a').click();
-
-
-        //NFA overview
-        cy.get('#ddlEventType').select("Request For Quotation");
-        cy.get('#txtEventref').type("RFQ test | 43{Enter}").wait(1000);
-
-        cy.get('#txtAmountFrom').type("1")
-        cy.get('#txtBudget').type("100000");
-        cy.get('#dropCurrency').select("INR");
-        cy.get('#txtProjectName').type("User is configuring Bid to ge quotes and select the vendor this Bid involves Scrap and metals and plastics");
-
-        //Puchase org
-        // cy.get('#ddlPurchaseOrg').select("")
-
-        //purchase group
-        // cy.get('#ddlPurchasegroup').select("")
-        
-
-        //Condition
-
-        cy.get('#ddlCondition').select("Condition 1")
-
-        // Remarks
-
-        cy.get('#txtRemark').type("User is configuring Bid to ge quotes and select the vendor this Bid involves Scrap and metals and plastics")
-
-        cy.get('.col-md-offset-5 > .btn-success').click();
-
-
-        //NFA Details
-
-        cy.get('#paramremark5').type("User is configuring Bid to ge quotes and select the vendor this Bid involves Scrap and metals and plastics");
-        cy.get('#paramremark6').type("User is configuring Bid to ge quotes and select the vendor this Bid involves Scrap and metals and plastics");
-        cy.get('#ddlNFAParam').select("Question 3");
-        cy.get('#addques').click();
-        cy.get('#paramremark32').type("User is configuring Bid to ge quotes and select the vendor this Bid involves Scrap and metals and plastics");
-        cy.get('.col-md-offset-5 > .btn-success').click();
-
-        
-      
-    
-
-
-      
-     });
-   
-    
-    
+    // Login
+    cy.get('#username').type("priya.chauhan@agileapt.com");
+    cy.get("#password").type("Vv@1234");
+    cy.get('.col-sm-8 > .btn').click();
   });
-  
+
+  it("User FA", () => {
+    // Navigate through menu
+    cy.get('.sidebar-toggler-wrapper > .sidebar-toggler').click();
+    cy.get('#ulMain > :nth-child(3) > [href="javascript:;"]').click();
+    cy.get('#MenuHeader1 > :nth-child(6) > a').click(); // NFA Overview
+
+    // Fill NFA form
+    cy.get('#txtTitle').type("test NFA"); // Enter Title
+    cy.get('#txtNFADetail').type("NFA Test"); // Enter Details
+    cy.get('#txtAmountFrom').type("1"); // Amount From
+    cy.get('#txtBudget').type("100000"); // Budget
+    cy.get('#dropCurrency').select("INR"); // Select Currency
+
+    // Purchase Organization Dropdown
+    cy.get(':nth-child(12) > :nth-child(1) > div.col-md-3').wait(8000);
+
+    //Purchase Group
+
+    cy.get('.col-md-3 > .col-md-10').click()
+
+    // Remarks (Rich Text Editor)
+    cy.get('.cke_wysiwyg_frame').then($iframe => {
+      const $body = $iframe.contents().find('body');
+      cy.wrap($body).type("User is configuring Bid...");
+    });
+
+    cy.get('.col-md-offset-5 > .btn-success').click(); // Submit Form
+
+    // NFA Details
+    cy.get('#paramremark17').type("Detail 1");
+
+    cy.get('.col-md-offset-5 > .btn-success').click(); // Final Submission
+
+    // Assertions
+    cy.get('.col-md-offset-5 > .green').click() 
+
+    cy.get('.bootbox > .modal-dialog > .modal-content > .modal-footer > .btn').click();
+  });
+});
